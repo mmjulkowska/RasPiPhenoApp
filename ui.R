7f55dfdd9e83b3be3b1fc74f9e9c9697e4b415bc
Magda Julkowska <magdalena.m.julkowska@gmail.com>
2022-08-26 09:55:38 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -4,18 +4,24 @@ ui <- fluidPage(
               
   tabPanel("Data Upload", icon=icon("upload"),
     sidebarPanel(
+      
+      selectizeInput("expType", label="What system did you use to collect data?",
+                     choices = c("PhenoRig", "PhenoCage"), multiple = F),
+      
       fileInput("csv.raspi",
-                label="Upload Raspberry Pi derives CSV files here",
+                label="Upload Raspberry Pi derived CSV files here",
                 multiple = TRUE),
+      
+      uiOutput("timeSTART"),
       
       fileInput("csv.meta",
                 label="Upload the decoding  Meta-data file here",
                 accept = c('text / csv', '.csv', 'text/comma-separated-values')),
+      
       checkboxInput("FWCheck", label = "My Meta-data contains collumn with Fresh Weight informatio", value = F),
       uiOutput("FWColumn"),
       
-      checkboxInput("SVCheck", label = "My RasPi data contains multiple side-view of one plant", value = F),
-      uiOutput("SVNumber"),
+      actionButton("MergeData", label = "Merge Data")
       # end of sidebar panel
     ),
   
@@ -29,20 +35,23 @@ ui <- fluidPage(
       tabPanel("Meta data", icon = icon("ruler"),
                verbatimTextOutput("uploaded_metadata_report"),
                dataTableOutput("Data_tabl2")),
-      tabPanel("Decoded data", icon = icon("magic"),
+      tabPanel("Decoded data", icon = icon("wand-magic-sparkles"),
                verbatimTextOutput("merged_data_report"),
-               dataTableOutput("merged_data"))
+               uiOutput("mergedtable_button"),
+               dataTableOutput("Data_tabl3"))
     ))
     # end of Tab1
   ),
   
   tabPanel("Vizual Curation", icon=icon("eye"),
     sidebarPanel(
+      uiOutput("Color_per")
              
     # end of sidebar panel
     ), 
     mainPanel(navbarPage("Visualization",
-      tabPanel("Timeseries Graph", icon=icon("chart-line"))
+      tabPanel("Timeseries Graph", icon=icon("chart-line"),
+               uiOutput("graph_over_time"))
     ))
     # end of Tab2
 )
